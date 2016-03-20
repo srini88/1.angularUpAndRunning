43aa4d7559bed2ed742fc2852d0b52f342dcfa47
srini88 <pvsrinivas.ece@gmail.com>
2016-03-20 18:42:32 -0500

    initial pilot ng-bind

---
+++ notes.js
@@ -0,0 +1,53 @@
+angular js is a framework , change your way to write your application, use bulit-in features 
+
+you do things in a certain way 
+
+javascript framework -
+allow you to create spas
+
+your browser will load a single page -- your users not gonna know the difference 
+
+site only loads the content it needs 
+
+
+ you manage pages using partials
+
+you are not loading different pages --loading different pieces of content 
+
+
+excellent templating engine - special tags 
+
+templates integral part of html - dont have to build separately - easier to use than special mustache js -- 
+
+handles DOM easily 
+
+event triggers --- data is directly bound to your template 
+
+also works with AJAX 
+
+MVC architecture -- 
+this approach with angular 
+
+Model - data (come from datastrucrue, db, json )
+views - how are displayed - done through template 
+
+controllers - glue that times models and views - 
+
+core features :
+directives - place in html starts with ng
+data binding --- {{}} tie form elements with data 
+filters -- data can be organized in a certain way 
+
+modules --- break up functionality into modules 
+routes -- heart of spa -- lets you divide your templates 
+
+controllers 
+
+
+angular js is lot more than adding basic functionality of html -- so better to put it at the top 
+
+
+
+The number one reason for using a CDN is to improve your user's experience in terms of speed, and as we know - speed matters!
+
+Ensuring a consistent experience for all your users is important.
